c3f11e71fb95fd22e2c125fd950654acc07211d8
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-05-12 01:09:54 -0300

    feat(tailwindcss):  change screens params

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    screens: {
+      xxs: "360px",
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px"
+    },
     extend: {}
   },
   plugins: []
