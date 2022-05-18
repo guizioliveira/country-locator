297432d8ab22499039a288364b96c07c1d85e320
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-05-17 23:53:24 -0300

    style(components): removing unused components

--- components/index.tsx
+++ components/index.tsx
@@ -2,6 +2,5 @@ export * from "./Header";
 export * from "./Filterbar";
 export * from "./CountriesList";
 import { CountryDetail } from "./CountryDetail/CountryDetail";
-import { DetailPage } from "./DetailPage/DetailPage";
 
-export { DetailPage, CountryDetail };
+export { CountryDetail };
