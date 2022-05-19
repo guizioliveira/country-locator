4c0db607029970604f44429a81ab9820932316b0
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-05-19 14:16:44 -0300

    feat(componetns): add "not found" mesage when have no filtered results

--- components/CountriesList/index.tsx
+++ components/CountriesList/index.tsx
@@ -1,25 +1,25 @@
-import React from "react";
-import { CountryCard } from "../CountryCard";
-import { Loading } from "../Loading";
-import { Country } from "../types";
+import React from "react"
+import { CountryCard } from "../CountryCard"
+import { Loading } from "../Loading"
+import { Country } from "../types"
 
 export interface CountriesListProps {
-  data: Country[];
-  loading: boolean;
-  setIsLoading: () => void;
+  data: Country[]
+  loading: boolean
+  setIsLoading: () => void
 }
 
 export const CountriesList: React.FC<CountriesListProps> = ({
   data,
   loading,
-  setIsLoading
+  setIsLoading,
 }) => {
   return (
     <div className="container mx-auto mt-96 md:mt-40 mb-6">
       <div className="flex items-center justify-center">
         {loading ? (
           <Loading />
-        ) : (
+        ) : data.length != 0 ? (
           <div className="w-full grid grid-cols-2 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-4 md:gap-7">
             {data.map((country, index) => (
               <CountryCard
@@ -29,8 +29,12 @@ export const CountriesList: React.FC<CountriesListProps> = ({
               />
             ))}
           </div>
+        ) : (
+          <span className="text-base-text font-bold text-lg">
+            No results for this search.
+          </span>
         )}
       </div>
     </div>
-  );
-};
+  )
+}
